7c44f2535a36e31526d3ddf8ab43296a324a5e1d
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-11-28 03:37:58 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+provider "ibm" {
+  region = "us-south"
+  zone   = "dal10"
+  ibmcloud_api_key = var.ibmcloud_api_key
+}
+
+
+terraform {
+  required_providers {
+    ibm = {
+      source  = "ibm-cloud/ibm"
+      version = ">= 1.85.0"
+    }
+  }
+}
+
