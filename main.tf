25b94efb6a56e9230fd73e13d3f3c2fb0e4538d7
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-11-28 03:30:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,13 @@ locals {
   pvs_cloud_instance_guid = split(":", data.ibm_resource_instance.pvs_workspace.id)[7]
 }
 
+# DATA SOURCE: Retrieve the ID of the specific 'Empty' image
+data "ibm_pi_image" "empty_os_image" {
+  # Choose one of the required empty deployment image names:
+  # "AIX-EMPTY", "IBMI-EMPTY", "RHEL-EMPTY", or "SLES-EMPTY"
+  pi_image_name = "IBMI-EMPTY" 
+  pi_cloud_instance_id = "crn:v1:bluemix:public:power-iaas:dal10:a/21d74dd4fe814dfca20570bbb93cdbff:cc84ef2f-babc-439f-8594-571ecfcbe57a::"
+
 
 
 # Get EXISTING PowerVS network
@@ -23,19 +30,17 @@ data "ibm_pi_network" "pvs_network" {
 }
 
 
-# Create AIX LPAR Clone
-resource "ibm_pi_instance" "clone" {
+# Create Empty IBMi LPAR Clone
+resource "ibm_pi_instance" "empty_lpar" {
   pi_cloud_instance_id = local.pvs_cloud_instance_guid
 
-  pi_instance_name = var.pvs_dr_instance_name
-  pi_image_id      = var.pvs_aix_image_id
+  pi_instance_name = var.pvs_instance_name
+  pi_image_id      = var.pvs_ibmi_image_id
 
-  pi_memory     = var.pvs_dr_instance_memory
-  pi_processors = var.pvs_dr_instance_cores
+  pi_memory     = var.pvs_instance_memory
+  pi_processors = var.pvs_instance_cores
   pi_proc_type  = "shared"
 
-  pi_sys_type     = "s922"
-  pi_storage_type = "tier3"
 
   # Use existing SSH key instead of creating one
   pi_key_pair_name = var.existing_key_name
